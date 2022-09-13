9b5a8f4f29360c406c950461fcfcb2fdb2ecac71
golden-expirirensu <gundari_dan@bk.ru>
2022-09-13 16:02:32 +0300

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,57 @@
+import './tasks';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-contract-sizer';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.16",
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bscTestnet: {
+      url: process.env.BCS_TESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygonMumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    avalancheFujiTestnet: {
+      url: process.env.AVAX_FUJI || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      rinkeby: process.env.ETHERSCAN_API_KEY ?? '',
+      bscTestnet: process.env.BSCSCAN_API_KEY ?? '',
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY ?? '',
+      avalancheFujiTestnet: process.env.AVASCAN_API_KEY ?? '',
+    },
+  },
+};
+
+export default config;
