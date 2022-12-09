1f9331b410cb6cf2730fab19cc3099e6a1cb98a2
golden-expiriensu <gundari_dan@bk.ru>
2022-12-09 16:21:01 +0700

    added deploy example

--- deploy/1_Contract.ts
+++ deploy/1_Contract.ts
@@ -1,18 +1,29 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+import { DEPLOY_ARGS } from "./config";
+import { verify } from "./helpers";
+
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
+  const args = [
+    DEPLOY_ARGS.CONTRACT.VALUE,
+    DEPLOY_ARGS.CONTRACT.IMMUTABLE_VALUE
+  ]
 
-  await deploy("ContractName", {
+  await deploy("Contract", {
     from: deployer,
-    args: [],
+    args,
     log: true,
   });
+
+  await verify(hre, "Contract", args);
 };
 
 export default func;
-func.tags = ["mainnet"];
+func.tags = ["mainnet", "Contract"];
+func.id = "Contract";
